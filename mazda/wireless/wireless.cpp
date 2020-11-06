254244620164b854bee76d875c7c58f4b6f1b500
Chris <silverchris@gmail.com>
2020-11-06 00:59:29 -0500

    initial wifi autoconfig

---
+++ mazda/wireless/wireless.cpp
@@ -0,0 +1,138 @@
+//
+// Created by silverchris on 2020-11-06.
+//
+
+#include "wireless.h"
+
+#include <dbus/dbus.h>
+#include <dbus-c++/dbus.h>
+#include <dbus-c++/glib-integration.h>
+#include <stdint.h>
+#include <string>
+#include <cstring>
+#include <functional>
+#include <condition_variable>
+#include <algorithm>
+#include <signal.h>
+#include <thread>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+#include "../dbus/generated_cmu.h"
+#include "hu.pb.h"
+
+#define LOGTAG "mazda-bt"
+
+#include "hu_uti.h"
+
+void sendMessage(int fd, google::protobuf::Message &message, uint16_t type) {
+    int byteSize = message.ByteSize();
+    uint16_t sizeOut = htobe16(byteSize);
+    uint16_t typeOut = htobe16(type);
+    char *out = (char *) malloc(byteSize + 4);
+    memcpy(out, &sizeOut, 2);
+    memcpy(out + 2, &typeOut, 2);
+
+    message.SerializeToArray(out + 4, byteSize);
+
+    auto written = write(fd, out, byteSize + 4);
+    if (written > -1) {
+        logi("Bytes written: %u\n", written);
+    } else {
+        logi("Could not write data\n");
+    }
+    free(out);
+}
+
+void handleWifiInfoRequest(int fd, uint8_t *buffer, uint16_t length) {
+    HU::WifiInfoRequest msg;
+    msg.ParseFromArray(buffer, length);
+    logi("WifiInfoRequest: %s\n", msg.DebugString().c_str());
+
+    HU::WifiInfoResponse response;
+    response.set_ip_address("192.168.53.1");
+    response.set_port(5000);
+    response.set_status(HU::WifiInfoResponse_Status_STATUS_SUCCESS);
+
+    sendMessage(fd, response, 7);
+}
+
+void handleWifiSecurityRequest(int fd, uint8_t *buffer, uint16_t length) {
+    HU::WifiSecurityReponse response;
+
+    response.set_ssid("MazdaCMU");
+    response.set_bssid("0c:d9:c1:8f:7f:bb");
+    response.set_key("password");
+    response.set_security_mode(HU::WifiSecurityReponse_SecurityMode_WPA2_PERSONAL);
+    response.set_access_point_type(HU::WifiSecurityReponse_AccessPointType_STATIC);
+
+    sendMessage(fd, response, 3);
+}
+
+void handleWifiInfoRequestResponse(int fd, uint8_t *buffer, uint16_t length) {
+    HU::WifiInfoResponse msg;
+    msg.ParseFromArray(buffer, length);
+    logi("WifiInfoResponse: %s\n", msg.DebugString().c_str());
+}
+
+void BDSClient::SignalConnected_cb(const uint32_t &type, const ::DBus::Struct <std::vector<uint8_t>> &data) {
+    char mac[18];
+    char pty[100];
+    char buf[100];
+    logi("Signal Connected:\n");
+    logi("\tType: %u\n", type);
+    std::copy(data._1.begin() + 14, data._1.begin() + 32, mac);
+    mac[18] = '\0';
+    logi("\tMAC: %s\n", mac);
+    logi("\tService ID: %u\n", data._1[36]);
+    if (data._1[36] == 15) {
+        std::strncpy(pty, (char *) &data._1[48], 100);
+        logi("\tPTY: %s\n", pty);
+        int fd = open(pty, O_RDWR | O_NOCTTY | O_SYNC);
+        HU::WifiInfoRequest request;
+        request.set_ip_address("192.168.53.1");
+        request.set_port(30515);
+
+        sendMessage(fd, request, 1);
+
+        logi("PTY opened\n");
+        ssize_t len = 0;
+        while (1) {
+            ssize_t i = read(fd, buf, 4);
+            len += i;
+            if (len >= 4) {
+                uint16_t size = be16toh(*(uint16_t *) buf);
+                uint16_t type = be16toh(*(uint16_t * )(buf + 2));
+                logi("Size: %u, MessageID: %u, left: %u\n", size, type);
+                if (len >= size + 4) {
+                    uint8_t *buffer = (uint8_t *) malloc(size);
+                    i = 0;
+                    while (i < size) {
+                        i += read(fd, buffer, size);
+                    }
+                    switch (type) {
+                        case 1:
+                            handleWifiInfoRequest(fd, buffer, size);
+                            break;
+                        case 2:
+                            handleWifiSecurityRequest(fd, buffer, size);
+                            break;
+                        case 7:
+                            handleWifiInfoRequestResponse(fd, buffer, size);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+static BDSClient *bds_client = NULL;
+
+void wireless_setup(DBus::Connection service_bus) {
+    bds_client = new BDSClient(service_bus, "/com/jci/bds", "com.jci.bds");
+}
+
---
+++ mazda/wireless/wireless.h
@@ -0,0 +1,121 @@
+//
+// Created by silverchris on 2020-11-06.
+//
+
+#ifndef HEADUNIT_WIRELESS_H
+#define HEADUNIT_WIRELESS_H
+class BDSClient : public com::jci::bds_proxy, public DBus::ObjectProxy
+{
+public:
+    BDSClient(DBus::Connection &connection, const char *path, const char *name): DBus::ObjectProxy(connection, path, name){}
+
+    virtual void Signaltest_cb(const uint32_t& type) override {}
+    virtual void SignalReady_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalNotReady_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDeleteInProgress_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDeleted_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDeleteAllInProgress_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDeleteAll_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDisconnectedAll_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceDisconnectionProgress_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceInquiryResult_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceNameStarted_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceNameSet_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceNameGet_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceAccessibilityGet_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDevicePair_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceServiceDiscovery_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceWrittenAll_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceAuthenticate_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalRemoteSupportedFeatures_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceGetConnectedDevices_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceLocalOobData_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceExtendedSearchResult_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceEnableStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceDisableStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalConnected_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDisconnected_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceFeatures_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceConfiguration_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkAppChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBatteryStatusChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBiased_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseConnect_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseDisonnect_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseFolderChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseFolderContent_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseFolderContents_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseSearch_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkBrowseUidsChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkContentAddToNowPlaying_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkContentAttributes_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkContentPlay_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkMediaPosition_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPanelOperation_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlaybackStatusChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerAddressedChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerBrowsedChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerItem_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerItems_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerItemsChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerNowContentChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkPlayerVolumeChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkStreamClosed_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkStreamConfigured_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkStreamLine_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkStreamMuted_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkStreamOpened_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkSupportedChanges_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkSupportedPlayerSetting_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkSystemStatusChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkTrackChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkTrackEnd_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkTrackMetaData_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalAudioSinkTrackStart_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientCallList_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientAudioConnect_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientAudioDisconnect_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientIndicator_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientOperator_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientSubscriber_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientMicVolumeChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientSpeakerVolumeChange_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientActivateVoiceDial_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientDeActivateVoiceDial_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientSiriSupportStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientSiriEyesFreeMode_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientSiriDisableNoiseReduction_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalPhoneStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientCmdError_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalCallClientScoCodecId_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientConfigured_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientFolderListing_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessageDeleted_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessageGotten_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessageListing_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessageNotification_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessagePushed_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalMessageClientMessageSetStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalNetworkClientConnected_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalNetworkClientDisconnected_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalPimClientImportStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalFileServerInfoEvent_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalFileServerFileAcceptanceStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalFileServerDataFileInProgressStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceLessAudioConnectStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceLessAudioDisconnectStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceLessAclConnectStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalServiceLessAclDisconnectStatus_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalBTChipFailure_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+    virtual void SignalDeviceAddressGet_cb(const uint32_t& type, const ::DBus::Struct< std::vector< uint8_t > >& data) override {}
+
+};
+
+void sendMessage(int fd, google::protobuf::Message &message, uint16_t type);
+void handleWifiInfoRequest(int fd, uint8_t *buffer, uint16_t length);
+void handleWifiSecurityRequest(int fd, uint8_t *buffer, uint16_t length);
+void handleWifiInfoRequestResponse(int fd, uint8_t *buffer, uint16_t length);
+void wireless_setup(DBus::Connection service_bus);
+
+#endif //HEADUNIT_WIRELESS_H
